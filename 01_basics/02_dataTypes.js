a43fa6ca77f27546d335797cb5fdc210ece72587
ujjwalprojects <77427598+ujjwalprojects@users.noreply.github.com>
2024-04-15 11:57:11 +0000

    lession-dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+/*treat all JS code as newer version, not required to 
+write but still make sense to write to ensure we are 
+using the newer version of JS.*/
+"use strict"; 
+
+// alert(3+3); alert is for the brower, here we are using node.js
+
+console.log(3+3);
+
+
+let name = "Ujjwal"
+let age = 30
+let isLoggedIn = false
+
+
+// Number := 2^53
+// bigint 
+// string := can be used using single quote or double quote
+// boolean
+// null := standalone value(type)
+        // let x = null
+// undefined := variable declared but not defined
+// symbol := usually required using react(unique)
+
+
+// object := 
+
+console.log(typeof "Mike") // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
