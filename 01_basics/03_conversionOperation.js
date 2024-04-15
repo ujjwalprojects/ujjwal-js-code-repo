48cb9170e7782d84ae0a74cc00d9d0ed7831910f
ujjwalprojects <77427598+ujjwalprojects@users.noreply.github.com>
2024-04-15 12:21:39 +0000

    lession-conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,62 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+// conversion of string to number
+let valueInNumber = Number(score);
+
+console.log(valueInNumber) //o/p := 33
+console.log(typeof valueInNumber) //o/p := number
+
+/************************/
+
+let age = "22aaa";
+
+let ageInNumber = Number(age);
+
+console.log(ageInNumber)  // o/p := NAN
+console.log(typeof ageInNumber) // o/p := number
+
+/***********************/
+let marks = null;
+
+let marksInNumber = Number(marks);
+
+console.log(marksInNumber)  // o/p := 0
+console.log(typeof marksInNumber) // o/p := number
+/***********************/
+
+let point = undefined;
+
+let pointInNumber = Number(point);
+
+console.log(pointInNumber)  // o/p := NAN
+console.log(typeof pointInNumber) // o/p := number
+
+/***********************/
+let qualified = true;
+
+let qualifiedInNumber = Number(qualified);
+
+console.log(qualifiedInNumber)  // o/p := 1
+console.log(typeof qualifiedInNumber) // o/p := number
+
+
+/***********************/
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn)  // o/p := true (if the input is greater than 1 then it fetches "true" as a result and only in case of 0 it fetches "false")
+console.log(typeof booleanIsLoggedIn) // o/p := boolean
+
+
+
+/***********************/
+let userEmail = 3333;
+
+let stringInNumber = String(userEmail);
+
+console.log(stringInNumber)  // o/p := 3333
+console.log(typeof stringInNumber) // o/p := string
