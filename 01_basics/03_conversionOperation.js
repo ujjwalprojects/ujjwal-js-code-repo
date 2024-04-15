e0a6c22d8d2093489d4346ae0688bdff7788a224
ujjwalprojects <77427598+ujjwalprojects@users.noreply.github.com>
2024-04-15 12:37:28 +0000

    lession-conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -60,3 +60,39 @@ let stringInNumber = String(userEmail);
 
 console.log(stringInNumber)  // o/p := 3333
 console.log(typeof stringInNumber) // o/p := string
+
+
+
+/********Operations****************/
+
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+console.log(2+2)
+console.log(2-2)
+console.log(2**3) //power
+console.log(2/3) //remainder
+console.log(2%3) //mod
+
+
+let str1 = "Hello"
+let str2 = " Ujjwal"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2) //:= "12"
+console.log("1"+2 +2) //:= "122"
+console.log(1+2 +"2") //:= "32" 
+console.log((1+2) +"2") //:= "32" 
+console.log(1+"2" +2) //:= "122"
+
+
+
+
+
+
+
