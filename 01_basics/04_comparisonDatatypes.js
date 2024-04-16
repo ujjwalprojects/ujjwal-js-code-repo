9f32365e48cffd084015584f1921263cc1f70b08
ujjwalprojects <77427598+ujjwalprojects@users.noreply.github.com>
2024-04-16 09:41:21 +0000

    lesson-comparisonDatatypes

--- 01_basics/04_comparisonDatatypes.js
+++ 01_basics/04_comparisonDatatypes.js
@@ -15,8 +15,6 @@ console.log(null>0); //false
 console.log(null==0); //false
 console.log(null>=0); //true
 
-
-
 console.log(undefined>0) //false
 console.log(undefined==0) //false
 console.log(undefined>=0) //false
