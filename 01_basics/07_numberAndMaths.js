7407458e45b09f2a688fe404c4011ef174640045
ujjwalprojects <77427598+ujjwalprojects@users.noreply.github.com>
2024-04-16 09:39:51 +0000

    lesson-numberAndMaths

--- 01_basics/07_numberAndMaths.js
+++ 01_basics/07_numberAndMaths.js
@@ -48,7 +48,7 @@ const min = 10
 const max = 20
 
 // this formula will give a random number between 10-20
-console.log(Math.floor(Math.random() * (max-min + 1))+min)
+console.log(Math.floor(Math.random() * (max-min + 1))+min) 
 
 
 
