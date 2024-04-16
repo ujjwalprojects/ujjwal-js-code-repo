fc18f483a1d82bb50fd3c6d4d9f38bb88ac18c95
ujjwalprojects <77427598+ujjwalprojects@users.noreply.github.com>
2024-04-16 06:16:24 +0000

    lesson-comparisonDatatypes

---
+++ 01_basics/04_comparisonDatatypes.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2" > 1); //true, because JS has converted this to number internally
+console.log("02" > 1); //true
+
+/*So when there is a comparison operation make sure we compare
+value of same datatype*/
+
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+
+
+
+console.log(undefined>0) //false
+console.log(undefined==0) //false
+console.log(undefined>=0) //false
+
+
+/**Strict Check  ===*/
+console.log("2" === 2) // false, because it checks the datatype as well
