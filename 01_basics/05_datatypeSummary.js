70742ce7d419d4af7cc25bc7c2304936c009abc0
ujjwalprojects <77427598+ujjwalprojects@users.noreply.github.com>
2024-04-16 06:38:47 +0000

    lession-datatypeSummary

---
+++ 01_basics/05_datatypeSummary.js
@@ -0,0 +1,45 @@
+// Primitive
+
+/**7 types:
+ *      String
+ *      Number
+ *      Boolean
+ *      null
+ *      undefined
+ *      Symbol     
+ *      BigInt
+ */
+
+const score = 100 //number
+const scoreValue = 100.3 //number
+const isLoggedIn = false // boolean
+const outSideTemp = null // null
+let userEmail; //undefined
+
+const id = Symbol('123')
+// console.log(id);
+
+// Reference type/Non-Primitive
+
+/**Array
+ * Objects
+ * Functions
+ */
+
+const brands = ["Toyota","Maruti"]
+
+let myObj ={
+    name:"Ujjwal",
+    gender:"Male",
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+
+
+
+
+
+
