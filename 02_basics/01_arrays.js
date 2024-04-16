e1fc6769d2ff0d586ca2a7338dcb1c554ed3ad02
ujjwalprojects <77427598+ujjwalprojects@users.noreply.github.com>
2024-04-16 11:16:43 +0000

    lesson-arraysInJS

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,89 @@
+// Array
+
+const myArr = ['A','B','C','D','E','F']
+// console.log(myArr[5])
+
+
+const carModels =['Maruti','Hundai']
+
+const myArr2 = new Array(1,3,5,6,7)
+// console.log(myArr2[2])
+// console.log(myArr2.length)
+
+//Array methods
+
+//add values to array
+myArr.push('singh')
+// console.log(myArr)
+
+//remove last element from array
+myArr.pop()
+// console.log(myArr)
+
+//put new element to start of the array
+myArr.unshift(8)
+// console.log(myArr)
+
+//pop first element an array
+myArr.shift()
+// console.log(myArr)
+
+
+// console.log(myArr.includes('ujj'))
+// console.log(myArr.indexOf('ujjwal'))
+
+
+const newArr3 = myArr.join()
+
+// console.log(newArr3)
+// console.log(typeof newArr3) //string
+
+
+
+// slice, splice
+
+/**
+ * The term slice literally means to cut something into pieces. 
+ * It is used to cut out elements from an array. 
+ * It does not affect the original array.
+ */
+/**array.slice(start, end)
+ * start denotes the index at which the array starts slicing.
+    end denotes the index at which the array stops slicing.
+ */
+
+
+// console.log('A', myArr)
+const myA1 = myArr.slice(0,2)
+// console.log('Slice ', myA1)
+
+/**
+ * Splice, according to the dictionary, 
+ * means to join things together. 
+ * It is used to remove elements from an array or replace them.
+ */
+/**
+ * array.splice(start, deleteCount, newElem1, newElem2, ..., newElemN;
+ * start denotes the index from which the method will start its operation on the array.
+ * deleteCount denotes the number of values to be deleted from the start. If the value is 0, nothing will be deleted.
+ * newElem1 to newElemN denote the values that would be added after the start.
+ */
+
+let fruits = ['apple', 'banana', 'cherry', 'date'];
+
+// Remove 1 element at index 2
+// fruits.splice(2, 3); 
+// Result: ['apple', 'banana', 'date']
+
+// Remove 2 elements starting from index 1 and insert 'orange', 'lemon'
+// fruits.splice(1, 2, 'orange', 'lemon'); 
+// Result: ['apple', 'orange', 'lemon', 'date']
+
+
+// Remove 0 elements starting from index 2 and insert 'grape'
+// fruits.splice(2, 0, 'grape'); 
+// Result: ['apple', 'orange', 'grape', 'lemon', 'date']
+
+
+console.log(fruits);
+
